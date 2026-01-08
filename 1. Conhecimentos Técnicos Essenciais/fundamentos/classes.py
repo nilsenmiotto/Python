a17a04437de8edb5e65c7dcf002e51c942c9bb4a
Nilsen Miotto <nilsen@routerbox.com.br>
2026-01-08 10:54:18 -0300

    Leitura sobre programação orientada a objetos com python

--- 1. Conhecimentos Técnicos Essenciais/fundamentos/classes.py
+++ 1. Conhecimentos Técnicos Essenciais/fundamentos/classes.py
@@ -14,3 +14,28 @@ class Pessoa:
 # Criando uma instância (objeto) da classe Pessoa
 pessoa1 = Pessoa("João", 25)
 pessoa1.apresentar() # Saída: Olá, meu nome é João e tenho 25 anos.
+
+# Outra classe para demonstrar herança
+class Curso:
+    def __init__(self, nome_curso, duracao):
+        self.nome_curso = nome_curso  # Atributo de instância
+        self.duracao = duracao  # Atributo de instância
+    
+    def detalhes_curso(self):  # Método de instância
+        print(f"Curso: {self.nome_curso}, Duração: {self.duracao} meses.")
+
+# Herança: criando uma classe que herda de outra
+class Estudante(Pessoa, Curso):
+    def __init__(self, nome, idade, nome_curso, duracao):
+        Pessoa.__init__(self, nome, idade)  # Chama o construtor da classe Pessoa
+        Curso.__init__(self, nome_curso, duracao)  # Chama o construtor da classe Curso
+
+    def apresentar(self):  # Sobrescrevendo o método apresentar
+        super().apresentar()  # Chama o método da classe base
+        print(f"Estou estudando {self.nome_curso}.")
+
+
+# Criando uma instância da classe Estudante
+estudante1 = Estudante("Maria", 22, "Engenharia", 48)
+estudante1.apresentar()
+estudante1.detalhes_curso()
